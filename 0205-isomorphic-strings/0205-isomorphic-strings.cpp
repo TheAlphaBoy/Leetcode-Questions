e0ec2682eed581e6f4e94ed5558d30bdccaafd55
Nimit Dubey <125803529+TheAlphaBoy@users.noreply.github.com>
2024-01-19 03:17:21 +0530

    Time: 5 ms (68.05%), Space: 7.2 MB (90.84%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+
+        // Base case
+        if(s.length() != t.length())
+        return false;
+
+        // Now length are equal for both string
+        // their are total 256 character including all upper and lower cases letter
+        int arr1[256]={};
+        int arr2[256]={};
+        for(int i=0;i<s.length();i++){
+            // if first time visiting then map and store it
+            if(!arr1[s[i]] && !arr2[t[i]]){
+                arr1[s[i]] = t[i];
+                arr2[t[i]] = s[i];
+            }
+            else{
+                if(arr1[s[i]] != t[i])
+                return false;
+            }
+        }
+        return true;
+    }
+};
