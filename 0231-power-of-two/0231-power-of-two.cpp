a00a27ebacafe002df93ba2c066c107f16d2c467
Nimit Dubey <125803529+TheAlphaBoy@users.noreply.github.com>
2024-01-26 00:34:44 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (6.69%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -11,12 +11,25 @@ public:
         // }
         // return false;
 
-        // sol 2 : using observation (Bit manipulation is done mainly by observation)
+        // Sol 2 : Recursive Approach
+
+        // // BC
+        // if(n==1) {
+        //     return true;
+        // }
+        // if(n%2 != 0 || n==0){
+        //     return false;
+        // }
+        // // Hypothesis (No Induction step , as function donot depend on previous function)
+        // // calling recursive function
+        // return isPowerOfTwo(n/2);
+
+        // sol 3 : using observation (Bit manipulation is done mainly by observation)
 
         // BC
         if(n<=0)
         return false;
-        //observe that by taking & operation b/w n-1 and n will always be zero if n is elemnt power of two 
+        // observe that by taking & operation b/w n-1 and n will always be zero if n is elemnt power of two 
 
         if((n&(n-1))==0)
         return true;
