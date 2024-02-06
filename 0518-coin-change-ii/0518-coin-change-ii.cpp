beebe06804835016745f559e2edee68a69e32d20
Nimit Dubey <125803529+TheAlphaBoy@users.noreply.github.com>
2024-02-06 19:44:58 +0530

    Time: 19 ms (50.23%), Space: 19.8 MB (26.72%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+
+        int n=coins.size();
+        vector<vector<int>>t(n+1,vector<int>(amount+1));
+        // Initiallising the dp
+        for(int i=0;i<n+1;i++){
+            for(int j=0;j<amount+1;j++){
+                t[0][j]=0;
+                t[i][0]=1;
+            }
+        }
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<amount+1;j++){
+                if(coins[i-1]<=j){
+                    t[i][j]=t[i][j-coins[i-1]] + t[i-1][j];
+                }
+                else{
+                    t[i][j]=t[i-1][j];
+                }
+            }
+        }
+        return t[n][amount];
+    }
+};
