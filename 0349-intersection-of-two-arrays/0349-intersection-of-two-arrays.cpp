a04328a52ae99cce71b4fbdc7bbd901027fe3dc9
Nimit Dubey <125803529+TheAlphaBoy@users.noreply.github.com>
2024-03-10 14:44:47 +0530

    Time: 0 ms (100.00%), Space: 12.5 MB (51.34%) - LeetHub

---
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+
+        // time complexity of O(n + m)
+
+        map<int,int>mp;
+        vector<int>ans;
+        for(int i=0;i<nums1.size();++i){
+            mp[nums1[i]]++;
+        }
+        for(int i=0;i<nums2.size();++i){
+            if(mp.find(nums2[i])!=mp.end()){
+                ans.push_back(nums2[i]);
+                mp.erase(nums2[i]);
+            }
+        }
+        return ans;
+    }
+};
