237bf41332a2358f01990272419ec8eb7a43bf2d
Nimit Dubey <125803529+TheAlphaBoy@users.noreply.github.com>
2024-03-14 01:01:15 +0530

    Time: 4 ms (35.26%), Space: 7.2 MB (41.86%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
@@ -2,32 +2,31 @@ class Solution {
 public:
     int pivotInteger(int n) {
 
-        // int sum=0;
-        // int bsum=0;
-        // for(int i=1;i<=n;++i){
-        //     if(n==1){
-        //         return 1;
-        //     }
-        //     sum += i;
-        //     if(sum==bsum){
-        //         return i+1;
-        //     }
-        //     else if(sum>bsum){
-        //         bsum += n;
-        //         --n;
-        //     }
-        // }
-        // return -1;
+        // Same TC and SC for both solution , TC => O(N) , and SC => O1)
+
+        // Sol 1 :
 
         // Ref : https://leetcode.com/problems/find-the-pivot-integer/solutions/4867096/beat-100-pivot-integer-finder
 
-        int ls=(n*(n+1))/2,rs=0;
+        // int ls=(n*(n+1))/2,rs=0;
 
-        while(ls>rs){
-        rs+=n;
-        if(rs==ls)return n;
-        ls-=n;
-        n--;
+        // while(ls>rs){
+        // rs+=n;
+        // if(rs==ls)return n;
+        // ls-=n;
+        // n--;
+        // }
+        // return -1;
+
+        // Sol 2 : 
+
+        int total_sum = n*(n+1)/2;
+        for(int i=1;i<=n;++i){
+            int left_sum = i*(i+1)/2;
+            int right_sum = total_sum - left_sum + i;
+            if(left_sum == right_sum){
+                return i;
+            }
         }
         return -1;
     }
