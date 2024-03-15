45acaaf229b160f0d34e68efc739ab87c2ee4bb6
Nimit Dubey <125803529+TheAlphaBoy@users.noreply.github.com>
2024-03-15 16:53:49 +0530

    Time: 14 ms (74.02%), Space: 27 MB (44.25%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -2,24 +2,45 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
 
-        int n = nums.size();
-        std::vector<int> answer(n, 1);
+        // TC => O(2*N) ~ O(N) , SC => O(2N) , as 2 extra array me store kar rhe hai memory,
+        // third wala to result ke liye hota hai to usko nhi lete hai
 
-        // Calculate the product of all elements to the left of i
-        int leftProduct = 1;
-        for (int i = 0; i < n; ++i) {
-            answer[i] *= leftProduct;
-            leftProduct *= nums[i];
+        // int n = nums.size();
+        // std::vector<int> answer(n, 1);
+
+        // // Calculate the product of all elements to the left of i
+        // int leftProduct = 1;
+        // for (int i = 0; i < n; ++i) {
+        //     answer[i] *= leftProduct;
+        //     leftProduct *= nums[i];
+        // }
+
+        // // Calculate the product of all elements to the right of i
+        // int rightProduct = 1;
+        // for (int i = n - 1; i >= 0; --i) {
+        //     answer[i] *= rightProduct;
+        //     rightProduct *= nums[i];
+        // }
+
+        // return answer;
+
+        // Revision code by self : 
+
+        int n=nums.size();
+        int prefix_prod[n];
+        int suffix_prod[n];
+        prefix_prod[0]=1;
+        suffix_prod[n-1]=1;
+        for(int i=1;i<n;++i){
+            prefix_prod[i] = prefix_prod[i-1]*nums[i-1];
         }
-
-        // Calculate the product of all elements to the right of i
-        int rightProduct = 1;
-        for (int i = n - 1; i >= 0; --i) {
-            answer[i] *= rightProduct;
-            rightProduct *= nums[i];
+        for(int i=n-2;i>=0;--i){
+            suffix_prod[i] = suffix_prod[i+1] * nums[i+1];
         }
-
-        return answer;
-        
+        vector<int>result(n);
+        for(int i=0;i<n;++i){
+            result[i] = prefix_prod[i] * suffix_prod[i];
+        }
+        return result;
     }
 };
