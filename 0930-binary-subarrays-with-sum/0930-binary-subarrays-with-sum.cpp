a09fa3aa852cfb75eb9cd0b37639d6bdc6132f1a
Nimit Dubey <125803529+TheAlphaBoy@users.noreply.github.com>
2024-03-15 12:51:56 +0530

    Time: 41 ms (47.89%), Space: 38.3 MB (20.76%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
@@ -2,48 +2,72 @@ class Solution {
 public:
     int numSubarraysWithSum(vector<int>& nums, int goal) {
 
-        // TLE
+        // Brute force Approach , TC => O(N*N)
 
-        // Time complexity: O(n)
-        // Space complexity: O(1)
-
-        // long long i=0;long long j=0;
-        // long long n=nums.size();
-        // long long sum=0;
-        // long long cnt=0;
-        // while(j<n){
-        //     sum += nums[j];
-        //     if(sum<goal){
-        //         ++j;
-        //     }
-        //     else if(sum==goal){
-        //         ++cnt;
-        //         sum -= nums[i];
-        //         ++i;++j;
-        //     }
-        //     else { // sum > goal
-        //     sum -= nums[i];
-        //     ++i;
+        // int ans = 0;
+        // int cnt = 0 ;
+        // int n = nums.size();
+        // for(int i = 0 ; i < n ; i ++){
+        //     ans = nums[i];
+        //     if(ans == goal)
+        //         cnt++;
+        //     for(int j = i + 1 ; j < n ; j++){
+        //         ans += nums[j];
+        //         if(ans == goal)
+        //             cnt++;
+        //         if(ans > goal)
+        //             break;
         //     }
         // }
         // return cnt;
 
-        int ans = 0;
-        int cnt = 0 ;
-        int n = nums.size();
-        for(int i = 0 ; i < n ; i ++){
-            ans = nums[i];
-            if(ans == goal)
-                cnt++;
-            for(int j = i + 1 ; j < n ; j++){
-                ans += nums[j];
-                if(ans == goal)
-                    cnt++;
-                if(ans > goal)
-                    break;
+        // sol 2 : Using Cummulative sum + hashmap approach
+
+        // Ref : https://youtu.be/5Quv9nnZs34?si=wGkCgr9nW3rLaA9M
+
+        // TC => O(N) bcz traverse only once , SC => O(N) bcz taken extra space as map
+
+        int curr_sum=0;
+        unordered_map<int,int>mp;
+        mp[0]=1; // i have seen current sum once
+        int ans=0;
+        for(int i=0;i<nums.size();++i){
+            curr_sum += nums[i];
+            int rem_sum = curr_sum - goal;
+            if(mp.find(rem_sum) != mp.end()){
+                ans += mp[rem_sum];
+            }
+            mp[curr_sum]++;
+        }
+        return ans;
+
+        // Sol 3 : Using Sliding Windows Approach
+
+        // TC => O(2*N) ~ O(N) && SC => O(1) , as no extra space taken
+
+        // Ref : https://youtu.be/5Quv9nnZs34?si=wGkCgr9nW3rLaA9M
+
+        int i=0;int j=0;
+        int n=nums.size();
+        int cnt_zeros = 0;
+        int result = 0;
+        int wind_sum = 0;
+        while(j<n){
+            wind_sum += nums[j];
+            while(i<j && (nums[i]==0 || wind_sum>goal)){
+                if(nums[i]==0){
+                    cnt_zeros++;
+                }
+                else{
+                    cnt_zeros=0;
+                }
+                wind_sum -= nums[i];
+                ++i;
+            }
+            if(wind_sum == goal){
+                result += 1+cnt_zeros;
             }
         }
-        return cnt;
-
+        return result;
     }
 };
