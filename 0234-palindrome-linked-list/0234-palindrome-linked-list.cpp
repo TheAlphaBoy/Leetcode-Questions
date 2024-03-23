902e5ce52782c2b2c22e3483c6eef4a9f6e96d93
Nimit Dubey <125803529+TheAlphaBoy@users.noreply.github.com>
2024-03-23 14:46:31 +0530

    Time: 154 ms (85.43%), Space: 118.9 MB (62.64%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -10,19 +10,84 @@
  */
 class Solution {
 public:
+    // bool isPalindrome(ListNode* head) {
+
+        //  Sol 1 : Brute Force
+        // TC => O(N) , SC => O(N)
+
+        // vector<int> v;            
+        // while(head != nullptr){                
+        //     v.push_back(head -> val);                
+        //     head = head -> next;              
+        // }         
+        // for(int i = 0; i < v.size() / 2; i++){                
+        //     if(v[i] != v[v.size() - i - 1]){                    
+        //         return false;                    
+        //     }                
+        // }            
+        // return true;
+
+    // }
+
+    // Sol 2 : Half ko reverse kardo and usko alag kardo linked list se and compare karo
+    // TC => O(N)
+    // SC => O(1)
+
     bool isPalindrome(ListNode* head) {
-
-        vector<int> v;            
-            while(head != nullptr){                
-                v.push_back(head -> val);                
-                head = head -> next;              
-            }         
-            for(int i = 0; i < v.size() / 2; i++){                
-                if(v[i] != v[v.size() - i - 1]){                    
-                    return false;                    
-                }                
-            }            
+        if(head==NULL || head->next==NULL){
             return true;
+        }
+        ListNode* slow=head;
+        ListNode* fast=head;
+        ListNode* prev=NULL;
 
+        while(fast && fast->next){
+            prev=slow;
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        ListNode* tail=reverseLL(slow);
+        prev->next=NULL;
+        while(tail != NULL && head != NULL){
+            if(tail->val != head->val){
+                return false;
+            }
+            tail=tail->next;
+            head=head->next;
+        }
+        return true;
     }
+    ListNode* reverseLL(ListNode* head){
+        if(head==NULL || head->next==NULL){
+            return head;
+        }
+        ListNode* newHead=reverseLL(head->next);
+        ListNode* front=head->next;
+        front->next=head;
+        head->next=NULL;
+        return newHead;
+    }
+
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
