fd5f61631f16b23da3481d148df38a4c4ba56620
Nimit Dubey <125803529+TheAlphaBoy@users.noreply.github.com>
2024-04-02 17:04:17 +0530

    Time: 6 ms (65.28%), Space: 8.2 MB (85.95%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -2,25 +2,76 @@ class Solution {
 public:
     bool isIsomorphic(string s, string t) {
 
-        // Base case
-        if(s.length() != t.length())
-        return false;
+        // // Base case
+        // if(s.length() != t.length())
+        // return false;
 
-        // Now length are equal for both string
-        // their are total 256 character including all upper and lower cases letter
+        // // Now length are equal for both string
+        // // their are total 256 character including all upper and lower cases letter
+        // int arr1[256]={};
+        // int arr2[256]={};
+        // for(int i=0;i<s.length();i++){
+        //     // if first time visiting then map and store it
+        //     if(!arr1[s[i]] && !arr2[t[i]]){
+        //         arr1[s[i]] = t[i];
+        //         arr2[t[i]] = s[i];
+        //     }
+        //     else{
+        //         if(arr1[s[i]] != t[i])
+        //         return false;
+        //     }
+        // }
+        // return true;
+
+        // Revision :
+
+        // Edge case :
+        if(s.length() != t.length()){
+            return false;
+        }
         int arr1[256]={};
         int arr2[256]={};
-        for(int i=0;i<s.length();i++){
-            // if first time visiting then map and store it
+        for(int i=0;i<s.length();++i){
             if(!arr1[s[i]] && !arr2[t[i]]){
-                arr1[s[i]] = t[i];
-                arr2[t[i]] = s[i];
+                arr1[s[i]]=t[i];
+                arr2[t[i]]=s[i];
             }
             else{
-                if(arr1[s[i]] != t[i])
-                return false;
+                if(arr1[s[i]] != t[i]){
+                    return false;
+                }
             }
         }
         return true;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
