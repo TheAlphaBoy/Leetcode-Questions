edef0778d68b69691db5445991bdbefcd00ecc37
Nimit Dubey <125803529+TheAlphaBoy@users.noreply.github.com>
2024-04-07 23:31:57 +0530

    Time: 3 ms (45.50%), Space: 7.4 MB (53.16%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -2,31 +2,80 @@ class Solution {
 public:
     bool checkValidString(string s) {
 
+        // Sol 1 : Using Stack
+
         // Time complexity:O(n)
         // Space complexity:O(n)
 
+        // int n=s.length();
+        // stack<int>open,star;
+        // for(int i=0;i<n;i++){
+        //     if(s[i]=='(')open.push(i);
+        //     else if(s[i]=='*')star.push(i);
+        //     else{ // s[i]==')'
+        //         if(!open.empty())open.pop();
+        //         else if(!star.empty())star.pop();
+        //         else return false;
+        //     }
+        // }
+        // while(!open.empty()){
+        //     if(star.empty())return false;
+        //     else{
+        //         if(open.top()>star.top())return false;
+        //         else{
+        //             star.pop();
+        //             open.pop();
+        //         }
+        //     }
+        // }
+        // return true;
+
         int n=s.length();
         stack<int>open,star;
-        for(int i=0;i<n;i++){
-            if(s[i]=='(')open.push(i);
-            else if(s[i]=='*')star.push(i);
-            else{ // s[i]==')'
-                if(!open.empty())open.pop();
-                else if(!star.empty())star.pop();
-                else return false;
+        for(int i=0;i<n;++i){
+            if(s[i]=='('){
+                open.push(i);
             }
-        }
-        while(!open.empty()){
-            if(star.empty())return false;
-            else{
-                if(open.top()>star.top())return false;
-                else{
-                    star.pop();
+            else if(s[i]=='*'){
+                star.push(i);
+            }
+            else{//s[i]==')'
+                if(!open.empty()){
                     open.pop();
+                }
+                else if(!star.empty()){
+                    star.pop();
+                }
+                else{
+                    return false;
                 }
             }
         }
+        while(!open.empty()){
+            if(star.empty()){
+                return false;
+            }
+            else if(open.top() > star.top()){
+                return false;
+            }
+            else{
+                open.pop();
+                star.pop();
+            }
+        }
         return true;
-
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
