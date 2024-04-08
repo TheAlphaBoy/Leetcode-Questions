27bcdae9cbeaa627dd00ebeb8630e226f8f5114c
Nimit Dubey <125803529+TheAlphaBoy@users.noreply.github.com>
2024-04-08 23:39:51 +0530

    Time: 0 ms (100.00%), Space: 10.7 MB (79.46%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.cpp
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.cpp
@@ -7,26 +7,61 @@ public:
         //T.C : O(n)
         //S.C : O(n)
         
-        int n = students.size();
-        stack<int>st;
-        queue<int>q;
-        for(int i=0;i<n;++i){
-            st.push(sandwiches[n-i-1]);
-            q.push(students[i]);
-        }
-        int noteat=0;
-        while(!q.empty() && noteat < q.size()){
-            if(st.top() == q.front()) {
-                q.pop();
-                st.pop();
-                noteat=0;
+        // int n = students.size();
+        // stack<int>st;
+        // queue<int>q;
+        // for(int i=0;i<n;++i){
+        //     st.push(sandwiches[n-i-1]);
+        //     q.push(students[i]);
+        // }
+        // int noteat=0;
+        // while(!q.empty() && noteat < q.size()){
+        //     if(st.top() == q.front()) {
+        //         q.pop();
+        //         st.pop();
+        //         noteat=0;
+        //     }
+        //     else{
+        //         q.push(q.front());
+        //         q.pop();
+        //         noteat++;
+        //     }
+        // }
+        // return noteat;
+
+        //Aproach-2 (Using counter only)
+        //T.C : O(n)
+        //S.C : O(1)
+
+        int stud_1=0;
+        int stud_0=0;
+        int n=students.size();
+        for(int it : students){
+            if(it==0){
+                stud_0++;
             }
             else{
-                q.push(q.front());
-                q.pop();
-                noteat++;
+                stud_1++;
             }
         }
-        return noteat;
+        for(int i=0;i<n;++i){
+            if(sandwiches[i]==0){
+                if(stud_0 > 0){
+                    stud_0--;
+                }
+                else{
+                    return stud_0 + stud_1;
+                }
+            }
+            else if(sandwiches[i]==1){
+                if(stud_1 > 0){
+                    stud_1--;
+                }
+                else{
+                    return stud_0 + stud_1;
+                }
+            }
+        }
+        return 0;
     }
 };
