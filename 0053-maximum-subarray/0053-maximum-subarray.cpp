7cd3d9b95a5f576c47ed0e4b57fab563cf91e89d
Nimit Dubey <125803529+TheAlphaBoy@users.noreply.github.com>
2024-06-15 23:44:31 +0530

    Time: 75 ms (67.74%), Space: 70.1 MB (66.99%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int curr = nums[0];
-        int result = nums[0];
-        
-        for (int i = 1; i < nums.size(); i++) {
-            curr = max(curr + nums[i], nums[i]);
-            result = max(result, curr);
-        }
-        
-        return result;
+        int n = nums.size();
+        //maximumSum will calculate our ans and currSumSubarray will calculate maximum sum subarray till ith position 
+        int maximumSum = INT_MIN, currSumSubarray = 0;
+        for (int i = 0; i < n; i++) {
+            currSumSubarray += nums[i]; 
+            maximumSum = max(maximumSum, currSumSubarray);
+            //here we are taking max with 0 bcz if currSumSubarray = -1 or any negative value then it again starts with currSumSubarray = 0
+            currSumSubarray = max(currSumSubarray, 0);
+        } 
+        return maximumSum;
     }
 };
