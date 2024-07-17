573ef0be6f6e7ee0d831db7ad4e43ae805512172
Nimit Dubey <125803529+TheAlphaBoy@users.noreply.github.com>
2024-07-17 23:34:07 +0530

    Time: 0 ms (100%), Space: 8.9 MB (8.79%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,17 +1,31 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        int n=matrix[0].size();
-        int arr[n][n];
-        for(int i=0;i<n;++i){
-            for(int j=0;j<n;++j){
-                arr[j][n-1-i] = matrix[i][j];
+
+        // Brute force : TC=>O(N^2) & SC=>O(N^2)
+
+        // int n=matrix[0].size();
+        // int arr[n][n];
+        // for(int i=0;i<n;++i){
+        //     for(int j=0;j<n;++j){
+        //         arr[j][n-1-i] = matrix[i][j];
+        //     }
+        // }
+        // for(int i=0;i<n;++i){
+        //     for(int j=0;j<n;++j){
+        //         matrix[i][j] = arr[i][j];
+        //     }
+        // }
+
+        // Sol 2 : Optimized sol : TC => O(N*N) , SC=>O(1)
+
+        int n = matrix.size();
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                swap(matrix[i][j], matrix[j][i]);
             }
+            reverse(matrix[i].begin(), matrix[i].end());
         }
-        for(int i=0;i<n;++i){
-            for(int j=0;j<n;++j){
-                matrix[i][j] = arr[i][j];
-            }
-        }
+
     }
 };
