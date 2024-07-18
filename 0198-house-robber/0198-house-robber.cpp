4f6a3e09f36f3ab8b16f88a6bc39b6f325e73512
Nimit Dubey <125803529+TheAlphaBoy@users.noreply.github.com>
2024-07-18 23:42:03 +0530

    Time: 4 ms (31.3%), Space: 9.4 MB (17.64%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,16 +1,31 @@
 class Solution {
 public:
+
+// Sol 1 : Recursive Approcah (TLE)
+    // int rob(vector<int>&nums, int i) {
+    // if (i < 0) {
+    //     return 0;
+    // }
+    // return max(rob(nums, i - 2) + nums[i], rob(nums, i - 1));
+    // }
+
+    // int rob(vector<int>& nums) {
+    //     return rob(nums,nums.size()-1);
+    // }
+
+    // Sol 2 : Tabulation Method (Buttom-Up DP)
+
     int rob(vector<int>& nums) {
-        int prev = 0;
-        int curr = 0;
-        int next = 0;
-        
-        for (int i = 0; i < nums.size(); i++) {
-            next = max(prev + nums[i], curr);
-            prev = curr;
-            curr = next;
+
+        if (nums.size() == 0) return 0;
+        vector<int>dp(nums.size()+1);
+        dp[0] = 0;
+        dp[1] = nums[0];
+        for (int i = 1; i < nums.size(); i++) {
+            int val = nums[i];
+            dp[i+1] = max(dp[i],dp[i-1] + val);
         }
-        
-        return curr;
+        return dp[nums.size()];
     }
+
 };
