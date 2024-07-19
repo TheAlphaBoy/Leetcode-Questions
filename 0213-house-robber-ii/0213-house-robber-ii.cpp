948d95fbec2da67d7014d9d73d0216da1bbaa77d
Nimit Dubey <125803529+TheAlphaBoy@users.noreply.github.com>
2024-07-19 23:29:46 +0530

    Time: 4 ms (32.51%), Space: 9.7 MB (10.38%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,30 +1,29 @@
 class Solution {
-
-    private:
-    int robber(vector<int>& nums, int start, int end) {
-        int prev = 0;
-        int curr = 0;
-        int next = 0;
-        
-        for (int i = start; i <= end; i++) {
-            next = max(prev + nums[i], curr);
-            prev = curr;
-            curr = next;
-        }
-        
-        return curr;
-    }
 public:
     int rob(vector<int>& nums) {
-        int n = nums.size();
+
+        int n=nums.size();
+		if(n==1)return nums[0];
+		vector<int>nums1,nums2,dp1(n-1),dp2(n-1);
+		for(int i=0;i<n;i++){
+			if(i!=0)nums1.push_back(nums[i]);
+			if(i!=n-1)nums2.push_back(nums[i]);
+		}
+		dp1[0]=nums1[0];
+		for(int i=1;i<n-1;i++){
+			int pick=nums1[i];
+			if(i!=1)pick+=dp1[i-2];
+			int notpick=dp1[i-1];
+			dp1[i]=max(pick,notpick);
+		}
+		dp2[0]=nums2[0];
+		for(int i=1;i<n-1;i++){
+			int pick=nums2[i];
+			if(i!=1)pick+=dp2[i-2];
+			int notpick=dp2[i-1];
+			dp2[i]=max(pick,notpick);
+		}
+		return max(dp1[n-2],dp2[n-2]); 
         
-        if (n == 1) {
-            return nums[0];
-        }
-        
-        int range1 = robber(nums, 0, n - 2);
-        int range2 = robber(nums, 1, n - 1);
-        
-        return max(range1, range2);
     }
 };
