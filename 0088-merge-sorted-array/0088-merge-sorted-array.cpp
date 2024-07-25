6d2657a50f8d78a18681dc101bf539bc6bc17bb3
Nimit Dubey <125803529+TheAlphaBoy@users.noreply.github.com>
2024-07-25 23:23:38 +0530

    Time: 3 ms (37.49%), Space: 11.4 MB (5.92%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,30 +1,31 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int j=0;
-        int i=0;
-        if(n==0) return;
-        if(m==0)
-        {  
-         for(int i = 0; i < n; i++){
-                nums1[i] = nums2[i];
-            } return;
-        } 
-        while(i<m)
-        {
-            if(nums1[i]>nums2[j])
-            {
-                swap(nums1[i],nums2[j]);
-                sort(nums2.begin(),nums2.end());
+
+        // Brute Force Approach : 
+
+        vector<int>ans;
+        int i=0,j=0;
+        while(i<m && j<n){
+            if(nums1[i]<=nums2[j]){
+                ans.push_back(nums1[i]);
+                ++i;
             }
-            i++;
+            else{
+                ans.push_back(nums2[j]);
+                ++j;
+            }
         }
-        j=0;
-        while(i<m+n)
-        {
-            nums1[i] = nums2[j];
-            j++;
-            i++;
+        while(i<m){
+            ans.push_back(nums1[i]);
+            ++i;
+        }
+        while(j<n){
+            ans.push_back(nums2[j]);
+            ++j;
+        }
+        for(int a=0;a<m+n;++a){
+            nums1[a]=ans[a];
         }
     }
 };
