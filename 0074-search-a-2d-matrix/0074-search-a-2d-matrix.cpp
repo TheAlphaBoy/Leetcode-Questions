ba1f34bdab737f9ecd6fb34f03a7c8246d1dfcc9
Nimit Dubey <125803529+TheAlphaBoy@users.noreply.github.com>
2024-07-27 23:45:29 +0530

    Time: 0 ms (100%), Space: 12.2 MB (10.42%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,15 +1,40 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int m=matrix.size();
-        if(m==0) return false;
-        int n=matrix[0].size();
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(matrix[i][j]==target)
+        int lowRow = 0;
+        int highRow = matrix.size() - 1;
+        
+        while (lowRow < highRow) {
+            int mid = lowRow + (highRow - lowRow) / 2;
+            if (matrix[mid][0] == target) {
                 return true;
             }
+            if (matrix[mid][0] < target && target < matrix[mid + 1][0]) {
+                lowRow = mid;
+                break;
+            }
+            if (matrix[mid][0] < target) {
+                lowRow = mid + 1;
+            } else {
+                highRow = mid - 1;
+            }
         }
+        
+        int lowCol = 0;
+        int highCol = matrix[0].size() - 1;
+        
+        while (lowCol <= highCol) {
+            int mid = lowCol + (highCol - lowCol) / 2;
+            if (matrix[lowRow][mid] == target) {
+                return true;
+            }
+            if (matrix[lowRow][mid] < target) {
+                lowCol = mid + 1;
+            } else {
+                highCol = mid - 1;
+            }
+        }
+        
         return false;
     }
 };
