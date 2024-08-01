d2f54f56e77ca4ad0d31c3be322cd87dcd162edc
Nimit Dubey <125803529+TheAlphaBoy@users.noreply.github.com>
2024-08-01 23:56:55 +0530

    Time: 3 ms (34.05%), Space: 8.1 MB (8.28%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,20 +1,34 @@
 class Solution {
 public:
+
+    // Sol 1 : Recursive Approach :
+    // int solve(int m,int n,int i,int j){
+    //     // BC 
+    //     if(i>m || j>n) return 0;
+    //     if(i==m-1 || j==n-1) return 1;
+    //     else return solve(m,n,i+1,j) + solve(m,n,i,j+1);
+    // }
+
+    // int uniquePaths(int m, int n) {
+
+    //     return solve(m,n,0,0);
+    // }
+
+    // Memoization Approach : (Optimized)
+
+    int solve(int m,int n,int i,int j,vector<vector<int>>&dp){
+        // Base case: if out of bounds
+        if (i >= m || j >= n) return 0;
+        // Base case: if reach the bottom-right cell
+        if (i == m-1 && j == n-1) return 1;
+        if(dp[i][j] != -1) return dp[i][j];
+        dp[i][j]=solve(m,n,i+1,j,dp) + solve(m,n,i,j+1,dp);
+        return dp[i][j];
+    }
+
     int uniquePaths(int m, int n) {
-        vector<vector<int>> grid(m, vector<int>(n, 0));
-        
-        for (int i = 0; i < m; i++) {
-            grid[i][0] = 1;
-        }
-        for (int j = 0; j < n; j++) {
-            grid[0][j] = 1;
-        }
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                grid[i][j] = grid[i - 1][j] + grid[i][j - 1];
-            }
-        }
-        
-        return grid[m - 1][n - 1];
+
+        vector<vector<int>>dp(m, vector<int>(n, -1));
+        return solve(m,n,0,0,dp);
     }
 };
