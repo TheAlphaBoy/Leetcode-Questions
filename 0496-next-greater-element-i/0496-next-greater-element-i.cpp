286e466b4725f7fb34d02f29fa5ac773cb6671f5
Nimit Dubey <125803529+TheAlphaBoy@users.noreply.github.com>
2024-08-12 03:07:33 +0530

    Time: 3 ms (82.96%), Space: 12.4 MB (15.43%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -2,7 +2,26 @@ class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
 
-        unordered_map<int,int>umap;
+        // unordered_map<int,int>umap;
+        // stack<int>st;
+        // for(int i=nums2.size()-1;i>=0;--i){
+        //     int elem=nums2[i];
+        //     while(!st.empty() && st.top()<=elem){
+        //         st.pop();
+        //     }
+        //     int ans=st.empty()?-1:st.top();
+        //     umap.insert({elem,ans});
+        //     st.push(elem);
+        // }
+        // vector<int>final;
+        // for(auto p:nums1){
+        //     final.push_back(umap[p]);
+        // }
+        // return final;
+
+        // Revision :
+
+        unordered_map<int,int>mp;
         stack<int>st;
         for(int i=nums2.size()-1;i>=0;--i){
             int elem=nums2[i];
@@ -10,13 +29,34 @@ public:
                 st.pop();
             }
             int ans=st.empty()?-1:st.top();
-            umap.insert({elem,ans});
+            mp.insert({elem,ans});
             st.push(elem);
         }
-        vector<int>final;
-        for(auto p:nums1){
-            final.push_back(umap[p]);
+        vector<int>res;
+        for(auto it:nums1){
+            res.push_back(mp[it]);
         }
-        return final;
+        return res;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
