2ba2735ceb0af18cfba2a8d45529725bc05a6f4b
Nimit Dubey <125803529+TheAlphaBoy@users.noreply.github.com>
2024-08-13 01:00:55 +0530

    Time: 0 ms (100%), Space: 9.8 MB (8.59%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,29 +1,35 @@
 class Solution {
 public:
-    int rob(vector<int>& nums) {
 
-        int n=nums.size();
-		if(n==1)return nums[0];
-		vector<int>nums1,nums2,dp1(n-1),dp2(n-1);
-		for(int i=0;i<n;i++){
-			if(i!=0)nums1.push_back(nums[i]);
-			if(i!=n-1)nums2.push_back(nums[i]);
-		}
-		dp1[0]=nums1[0];
-		for(int i=1;i<n-1;i++){
-			int pick=nums1[i];
-			if(i!=1)pick+=dp1[i-2];
-			int notpick=dp1[i-1];
-			dp1[i]=max(pick,notpick);
-		}
-		dp2[0]=nums2[0];
-		for(int i=1;i<n-1;i++){
-			int pick=nums2[i];
-			if(i!=1)pick+=dp2[i-2];
-			int notpick=dp2[i-1];
-			dp2[i]=max(pick,notpick);
-		}
-		return max(dp1[n-2],dp2[n-2]); 
+     // Ref : https://leetcode.com/problems/house-robber-ii/solutions/5362556/tabulation-approach-c-o-n-complexity
+
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 1) return nums[0];
+
+        vector<int> dp2(n);
+        dp2[n-1] = nums[n-1];
+        dp2[n-2] = max(nums[n-1],nums[n-2]);
+
+        for(int i= n - 3 ;i>=0;i--){
+            if(i==0){
+                dp2[0] = max( dp2[1] , dp2[2]) ;
+            }
+            else{
+                dp2[i] = max(dp2[i+1], dp2[i+2] + nums[i]);
+            }
+        }
+        vector<int> dp(n);
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
         
+        for (int i = 2; i < n; i++) {
+            if(i==n-1 ){
+                dp[i] = max(dp[i-1] , dp[n-2]) ;
+            }
+            else
+                dp[i] = max(dp[i-1], dp[i-2] + nums[i]);
+        }
+       return  max(dp2[0],dp[n-1]);
     }
 };
