0e990e9d7a9ae86f981258288ffbc82a2541ea07
Nimit Dubey <125803529+TheAlphaBoy@users.noreply.github.com>
2024-08-23 01:03:21 +0530

    Time: 3 ms (41.07%), Space: 7.6 MB (33.08%) - LeetHub

---
+++ 0476-number-complement/0476-number-complement.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int findComplement(int num) {
+        int num_bits = (int) (log2(num))+1;
+        // jitne bits hai utne 1 ho isloye mask liya hai
+        // As int lene par interger overflow ho rha hai isliye unsigned int le rhe h
+        unsigned int mask = (1U<<num_bits)-1;
+        return num^mask;
+    }
+};
