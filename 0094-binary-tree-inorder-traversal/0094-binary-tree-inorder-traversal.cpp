35fd45f7582f22bbe3b2f9ed757d3dc758224fda
Nimit Dubey <125803529+TheAlphaBoy@users.noreply.github.com>
2024-09-07 01:21:43 +0530

    Time: 0 ms (100%), Space: 10.1 MB (25.04%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -11,22 +11,43 @@
  */
 class Solution {
 public:
+    // vector<int> inorderTraversal(TreeNode* root) {
+
+    //     // Brute Force : Recursive Approach ( you can also use explicit stack Approach)
+    //     vector<int>inorder;
+
+    //     solve(root,inorder);
+    //     return inorder;     
+    // }
+    // void solve(TreeNode* root,vector<int>&inorder){
+    //     // Edge Case
+    //     if(root==NULL){
+    //         return;
+    //     }
+
+    //     solve(root->left,inorder);
+    //     inorder.push_back(root->val);
+    //     solve(root->right,inorder);
+    // }
+
+    // Sol 2 : Interative Approach
+
     vector<int> inorderTraversal(TreeNode* root) {
+        stack<TreeNode*> stk;
+        vector<int> path;
+        TreeNode* current = root;
 
-        // Recursive Approach ( you can also use explicit stack Approach)
-        vector<int>inorder;
-
-        solve(root,inorder);
-        return inorder;     
-    }
-    void solve(TreeNode* root,vector<int>&inorder){
-        // Edge Case
-        if(root==NULL){
-            return;
+        while (!stk.empty() || current != NULL) {
+            while (current != NULL) {
+                stk.push(current);
+                current = current->left;
+            }
+            TreeNode* node = stk.top();
+            path.push_back(node->val);
+            stk.pop();
+            current = node->right;
         }
-
-        solve(root->left,inorder);
-        inorder.push_back(root->val);
-        solve(root->right,inorder);
+        return path;
     }
+
 };
