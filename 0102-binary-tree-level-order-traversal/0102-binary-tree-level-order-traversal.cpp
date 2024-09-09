1a14467bbb5faafe47044a44eb8923bb7fa8f07f
Nimit Dubey <125803529+TheAlphaBoy@users.noreply.github.com>
2024-09-10 01:45:07 +0530

    Time: 10 ms (12.07%), Space: 15.2 MB (65.37%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -13,6 +13,32 @@ class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
 
+        // vector<vector<int>>ans;
+        // // Edge case
+        // if(root==NULL){
+        //     return ans;
+        // }
+        // queue<TreeNode*>q;
+        // q.push(root);
+        // while(!q.empty()){
+        //     int size = q.size();
+        //     vector<int>level;
+        //     for(int i=0;i<size;++i){
+        //         TreeNode* node = q.front();
+        //         q.pop();
+        //         if(node->left!=NULL){
+        //             q.push(node->left);
+        //         }
+        //         if(node->right!=NULL){
+        //             q.push(node->right);
+        //         }
+        //         level.push_back(node->val);
+        //     }
+        //     ans.push_back(level);
+        // }
+        // return ans;
+
+        // Revision : 
         vector<vector<int>>ans;
         // Edge case
         if(root==NULL){
@@ -21,15 +47,15 @@ public:
         queue<TreeNode*>q;
         q.push(root);
         while(!q.empty()){
-            int size = q.size();
+            int size=q.size();
             vector<int>level;
-            for(int i=0;i<size;++i){
+            for(int i=0;i<size;i++){
                 TreeNode* node = q.front();
                 q.pop();
-                if(node->left!=NULL){
+                if(node->left != NULL){
                     q.push(node->left);
                 }
-                if(node->right!=NULL){
+                if(node->right != NULL){
                     q.push(node->right);
                 }
                 level.push_back(node->val);
@@ -39,3 +65,33 @@ public:
         return ans;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
