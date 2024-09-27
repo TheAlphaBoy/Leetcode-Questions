145c1d4175f883a6c4e125bb2707c5d1f7515ffd
Nimit Dubey <125803529+TheAlphaBoy@users.noreply.github.com>
2024-09-28 01:46:06 +0530

    Time: 64 ms (53.3%), Space: 13.9 MB (57.33%) - LeetHub

--- 3573-count-substrings-that-can-be-rearranged-to-contain-a-string-i/3573-count-substrings-that-can-be-rearranged-to-contain-a-string-i.cpp
+++ 3573-count-substrings-that-can-be-rearranged-to-contain-a-string-i/3573-count-substrings-that-can-be-rearranged-to-contain-a-string-i.cpp
@@ -1,35 +1,40 @@
 class Solution {
 public:
-    long long validSubstringCount(string s, string t) {
-        vector<int>a(26,0);
-        vector<int>b(26,0);
-        for(auto x : t){
-            b[x-'a']++;
-        }
-        int k=0;
-        long long  count=0;
-        int i=0;
-        bool flag=false;
-        for( i=0; i<s.length(); i++){
+    long long validSubstringCount(string word1, string word2) {
+        int size1 = word1.size();
+        int size2 = word2.size();
+        // store char frequency for word2
+        vector<int> freq2(26, 0);   // only lowercase letters
+        for(auto c: word2)
+            freq2[c - 'a']++;
+        
+        // Sliding Window
+        long long ans = 0;
+        vector<int> freq(26, 0);
+        for(int l=0, r=0; r<size1; r++) {
+            // expand right
+            freq[word1[r] - 'a']++;
 
-            if(flag==false)
-            a[s[i]-'a']++;
-             flag = true;
-            for(int i=0; i<26; i++){
-                if(a[i]<b[i]){
-                    flag = false;
-                    break;
-                }
-            }
-            if(flag){
-                count+=s.length()-i;
-                a[s[k]-'a']--;
-                k++;
-                i--;
-            }
-            
+            // Compare -> update ans -> shrink left
+            while(compare(freq, freq2)) {
+                ans += size1 - r;   // Update ans
+                // Shrink left
+                freq[word1[l] - 'a']--;
+                l++;  
+            }        
         }
-       
-        return count;
+        return ans;
+    }
+    bool compare(vector<int>& freq1, vector<int>& freq2){
+        int k = 0;
+        while(k <= 25){
+            if(freq1[k] >= freq2[k])
+                k++;
+            else 
+                break;
+        }
+        if(k == 26) 
+            return true;
+        return false;
     }
 };
