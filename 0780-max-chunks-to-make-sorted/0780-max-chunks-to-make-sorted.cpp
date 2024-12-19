2c39a4fa755f1e4cbc92a5a768afe6efaf183111
Nimit Dubey <125803529+TheAlphaBoy@users.noreply.github.com>
2024-12-20 02:37:26 +0530

    Time: 0 ms (100%), Space: 9.3 MB (21.11%) - LeetHub

---
+++ 0780-max-chunks-to-make-sorted/0780-max-chunks-to-make-sorted.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxChunksToSorted(vector<int>& arr) {
+        int ans=0;
+        int maxi=-1;
+        for(int i=0;i<arr.size();i++){
+             maxi=max(maxi,arr[i]);
+            if(maxi==i)ans++;
+    }
+    return ans;
+    }
+};
