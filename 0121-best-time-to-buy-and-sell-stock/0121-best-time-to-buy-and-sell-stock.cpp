6c7bb86cc580811e64e9d128abca2cd788615faf
Nimit Dubey <125803529+TheAlphaBoy@users.noreply.github.com>
2025-07-01 02:21:52 +0530

    Time: 7 ms (6.84%), Space: 97.2 MB (99%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,16 +1,25 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int n=prices.size();
-        // Edge case
-        if(n==0)
-        return 0;
-        int min_price=prices[0];
-        int max_profit=0;
-        for(int i=1;i<n;++i){
-            max_profit=max(max_profit,prices[i]-min_price);
-            min_price=min(min_price,prices[i]);
+
+        // Brute Force (TLE) -> Two pointers approach
+        // int ans = 0;
+        // for(int i=0;i<prices.size()-1;++i) {
+        //     for(int j=i+1;j<prices.size();++j) {
+        //         ans = max(prices[j]-prices[i],ans);
+        //     }
+        // }
+        // return ans;
+
+        // Optimized sol -> without DP
+
+        int maxPro = 0;
+        int minPrice = INT_MAX;
+        for(int i = 0; i < prices.size(); i++){
+            minPrice = min(minPrice, prices[i]);
+            maxPro = max(maxPro, prices[i] - minPrice);
         }
-        return max_profit;
+        return maxPro;
+
     }
 };
