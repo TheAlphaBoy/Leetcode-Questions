91de1cb5999a12c4061c2b72f194d1a1b295ff6f
Nimit Dubey <125803529+TheAlphaBoy@users.noreply.github.com>
2025-08-28 02:45:47 +0530

    Time: 0 ms (100%), Space: 9.6 MB (64.49%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,39 +1,25 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        
-        vector<vector<int>> ans;
-        if(numRows <= 0){
-            return ans;
-        }
 
-            ans.push_back(vector<int>(1,1));
+        vector<vector<int>>ans;
 
-        if(numRows == 1){
-            return ans;
-        }
-            ans.push_back(vector<int>(2 , 1));
+        if(numRows==0) return ans;
+        ans.push_back(vector<int>(1,1));
+        if(numRows==1) return ans;
+        ans.push_back(vector<int>(2,1));
 
+        if(numRows==2) return ans;
 
-        if(numRows == 2){
-            return ans;
-        }
-
-      
-
-        for(int  i = 2; i < numRows; i ++){
-            vector<int> temp;
-
-      temp.push_back(1);
-            int previousRow = i-1;
-
-            for(int j = 0; j < ans[previousRow].size()-1;j++){
-                temp.push_back(ans[previousRow][j]+ans[previousRow][j+1]);
+        for(int i=2;i<numRows;++i){
+            vector<int>temp;
+            temp.push_back(1);
+            int prev = i-1;
+            for(int j=0;j<ans[prev].size()-1;++j){
+                temp.push_back(ans[prev][j] + ans[prev][j+1]);
             }
-
-                temp.push_back(1);
-
-                ans.push_back(temp);
+            temp.push_back(1);
+            ans.push_back(temp);
         }
 
         return ans;
